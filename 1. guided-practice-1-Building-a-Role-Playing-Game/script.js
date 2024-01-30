5d25fd7ea33b15bdae3f5122b5fa0097840ca51d
AlexTD89 <trudov.alexandru@gmail.com>
2024-01-30 00:04:00 -0500

    complete update(), edit goStore()

--- 1. guided-practice-1-Building-a-Role-Playing-Game/script.js
+++ 1. guided-practice-1-Building-a-Role-Playing-Game/script.js
@@ -42,14 +42,22 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
-
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+  text.innerText = location.text;
 }
 
 function goTown() {
   update(locations[0]);
 }
 
-function goStore() {}
+function goStore() {
+  update(locations[1]);
+}
 
 function goCave() {
   console.log("Going to cave.");
