0b44de0294fcf1982d2e5aaedb9a546ddf31c70d
AlexTD89 <trudov.alexandru@gmail.com>
2024-02-01 22:16:49 -0500

    update buyWeapon()

--- 1. guided-practice-1-Building-a-Role-Playing-Game/script.js
+++ 1. guided-practice-1-Building-a-Role-Playing-Game/script.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 let xp = 0;
 let health = 100;
 let gold = 50;
@@ -16,6 +17,12 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [
+  { name: 'stick', power: 5 },
+  { name: 'dagger', power: 30 },
+  { name: 'claw hammer', power: 50 },
+  { name: 'sword', power: 100 }
+];
 const locations = [
   {
     name: "town square",
@@ -80,8 +87,26 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-
+  if (currentWeapon < weapons.length - 1) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
+  }
 }
+
+
 
 function fightSlime() {
 
