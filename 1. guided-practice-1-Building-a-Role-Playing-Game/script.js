4c6cc260a929f2b6c054ef1f6adc87786c18255e
AlexTD89 <trudov.alexandru@gmail.com>
2024-02-04 23:19:37 -0500

    update isMonsterHit()

--- 1. guided-practice-1-Building-a-Role-Playing-Game/script.js
+++ 1. guided-practice-1-Building-a-Role-Playing-Game/script.js
@@ -206,9 +206,9 @@ function getMonsterAttackValue(level) {
   return hit > 0 ? hit : 0;
 }
 
-function isMonsterHit(){
-  return (Math.random() > .2);
-}
+function isMonsterHit() {
+    return Math.random() > .2 || health < 20;
+  }
 
 function dodge() {
   text.innerText = "You dodge the attack from the " + monsters[fighting].name;
